d26bfd0ba82536a9a454fda30647c567288b9653
1kolasik1 <kolasikpivasik@gmail.com>
2022-12-08 13:09:31 +0300

    create base project

--- src/routes/Images.jsx
+++ src/routes/Images.jsx
@@ -3,63 +3,63 @@ export default function Images({ src }) {
     <div className="grid grid-cols-3 justify-center">
       <div className="my-0 mx-auto">
         <img
-          className="w-[150px] h-[150px] my-[20px]"
+          className="w-[10vh] h-[10vh] my-[20px] mx-[5px] sm:w-[150px] sm:h-[150px] sm:my-[20px] sm:mx-0"
           src={src}
           alt="img"
         ></img>
       </div>
       <div className="my-0 mx-auto">
         <img
-          className="w-[150px] h-[150px] my-[20px]"
+          className="w-[10vh] h-[10vh] my-[20px] mx-[5px] sm:w-[150px] sm:h-[150px] sm:my-[20px] sm:mx-0"
           src={src}
           alt="img"
         ></img>
       </div>
       <div className="my-0 mx-auto">
         <img
-          className="w-[150px] h-[150px] my-[20px]"
+          className="w-[10vh] h-[10vh] my-[20px] mx-[5px] sm:w-[150px] sm:h-[150px] sm:my-[20px] sm:mx-0"
           src={src}
           alt="img"
         ></img>
       </div>
       <div className="my-0 mx-auto">
         <img
-          className="w-[150px] h-[150px] my-[20px]"
+          className="w-[10vh] h-[10vh] my-[20px] mx-[5px] sm:w-[150px] sm:h-[150px] sm:my-[20px] sm:mx-0"
           src={src}
           alt="img"
         ></img>
       </div>
       <div className="my-0 mx-auto">
         <img
-          className="w-[150px] h-[150px] my-[20px]"
+          className="w-[10vh] h-[10vh] my-[20px] mx-[5px] sm:w-[150px] sm:h-[150px] sm:my-[20px] sm:mx-0"
           src={src}
           alt="img"
         ></img>
       </div>
       <div className="my-0 mx-auto">
         <img
-          className="w-[150px] h-[150px] my-[20px]"
+          className="w-[10vh] h-[10vh] my-[20px] mx-[5px] sm:w-[150px] sm:h-[150px] sm:my-[20px] sm:mx-0"
           src={src}
           alt="img"
         ></img>
       </div>
       <div className="my-0 mx-auto">
         <img
-          className="w-[150px] h-[150px] my-[20px]"
+          className="w-[10vh] h-[10vh] my-[20px] mx-[5px] sm:w-[150px] sm:h-[150px] sm:my-[20px] sm:mx-0"
           src={src}
           alt="img"
         ></img>
       </div>
       <div className="my-0 mx-auto">
         <img
-          className="w-[150px] h-[150px] my-[20px]"
+          className="w-[10vh] h-[10vh] my-[20px] mx-[5px] sm:w-[150px] sm:h-[150px] sm:my-[20px] sm:mx-0"
           src={src}
           alt="img"
         ></img>
       </div>
       <div className="my-0 mx-auto">
         <img
-          className="w-[150px] h-[150px] my-[20px]"
+          className="w-[10vh] h-[10vh] my-[20px] mx-[5px] sm:w-[150px] sm:h-[150px] sm:my-[20px] sm:mx-0"
           src={src}
           alt="img"
         ></img>
--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -3,11 +3,11 @@ import { NavLink, Outlet } from "react-router-dom";
 export default function Layout() {
   const setActive = ({ isActive }) =>
     isActive
-      ? "font-bold ml-[20px] text-white ease-out underline"
-      : "ease-out ml-[20px] font-regular text-gray-700 underline decoration-grey-400";
+      ? "ml-0 font-bold sm:font-bold mx-[20px] text-white ease-out underline"
+      : "ml-0 font-bold sm:ease-out mx-[20px] font-regular text-gray-700 underline decoration-grey-400";
   return (
     <div className="min-h-screen">
-      <header className="bg-gray-900 pl-[20px] pr-[20px] pt-[30px] pb-[20px] h-[10vh] text-3xl">
+      <header className="flex justify-between bg-gray-900  sm:block bg-gray-900 pl-[20px] pr-[20px] pt-[30px] pb-[20px] h-[10vh] text-3xl">
         <NavLink to="/albums" end={true} className={setActive}>
           Albums
         </NavLink>
@@ -18,16 +18,19 @@ export default function Layout() {
       <main>
         <Outlet />
       </main>
-      <footer className="sticky top-full h-[10vh] mx-[100px] my-[20px]">
-        <div className="flex justify-between border-solid border-t-4 border-t-black font-bold text-2xl">
-          <div className="flex">
+      <footer className="mx-0 sticky top-full h-[10vh]  my-[20px] sm:mx-[100px]">
+        <div className="flex justify-between border-solid border-t-2 border-t-black font-bold text-2xl sm:border-t-4">
+          <div className="flex text-gray-400">
             <div>Created By:</div>
-            <a className="ml-5" href="https://vk.com/kolasikpivasik">
+            <a
+              className="ml-5 text-gray-400"
+              href="https://vk.com/kolasikpivasik"
+            >
               nikgor
             </a>
           </div>
-          <div className="bsu">
-            <a className="linkToBsu" href="https://bsu.by">
+          <div className="text-gray-400">
+            <a className="text-gray-400" href="https://bsu.by">
               BSU 2022
             </a>
           </div>
