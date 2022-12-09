a1456979883c2109c92a84447d6ee00a7bd30e3f
1kolasik1 <kolasikpivasik@gmail.com>
2022-12-09 19:46:08 +0300

    styles fixed

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout() {
       : "ml-0 font-bold sm:ease-out mx-[20px] font-regular text-gray-700 underline decoration-grey-400";
   return (
     <div className="min-h-screen">
-      <header className="flex justify-between bg-gray-900  sm:block bg-gray-900 pl-[20px] pr-[20px] pt-[30px] pb-[20px] h-[10vh] text-3xl">
+      <header className="flex justify-between  sm:block bg-gray-900 pl-[20px] pr-[20px] pt-[30px] pb-[20px]  text-3xl">
         <NavLink to="/albums" end={true} className={setActive}>
           Albums
         </NavLink>
@@ -18,20 +18,15 @@ export default function Layout() {
       <main>
         <Outlet />
       </main>
-      <footer className="mx-0 sticky top-full h-[10vh]  my-[20px] sm:mx-[100px]">
+      <footer className="mx-0 sticky top-full   my-[20px] sm:mx-[100px]">
         <div className="flex justify-between border-solid border-t-2 border-t-black font-bold text-2xl sm:border-t-4">
-          <div className="flex text-gray-400">
+          <div className="flex justify-center items-center">
             <div>Created By:</div>
             <a
               className="ml-5 text-gray-400"
               href="https://vk.com/kolasikpivasik"
             >
               nikgor
-            </a>
-          </div>
-          <div className="text-gray-400">
-            <a className="text-gray-400" href="https://bsu.by">
-              BSU 2022
             </a>
           </div>
         </div>
